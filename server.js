1b45874ecea6902935bd35cb9cb3427d3b4ecda9
Adish03101 <adishsajeev01@gmail.com>
2025-04-09 17:09:40 +0530

    never again

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = 80085;
+// express is a web framework
+// staring a server
 
-app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
+const express = require('express');// 'requires' a package for te server
+const app = express();// defining our backedn application
+const PORT = 8008;//subderectory of the device which is accessing(ip add of the device)
 
+console.log(`extra line`);
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));// backend is an hardware which runs a software which connects to internet which listens to the incomming queryS
