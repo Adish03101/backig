85f12493a207e930e8a4489e2a56a04260a87ec6
Adish03101 <adishsajeev01@gmail.com>
2025-04-10 20:46:11 +0530

    commits

--- proj2/src/routes/todoRoutes.js
+++ proj2/src/routes/todoRoutes.js
@@ -3,11 +3,29 @@ import db from '../db.js'
 
 const router = express.Router()
 
-router.get('/', (req,res) => { })
+// get todos for authenticated user
+router.get('/', (req, res) => {
+    const gettodo = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = gettodo.all(req.userId)
+    res.json(todos)
+})
 
-//update todo
-router.put('/:id', (req,res) => {})
+// create new todo
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare('INSERT INTO todos (user_id, task) VALUES (?, ?)')
+    const result = insertTodo.run(req.userId, task)
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+})
 
-router.delete('/:id',(req,res) => {})
+// update todo (to be implemented)
+router.put('/:id', (req, res) => {
+    
+})
+
+// delete todo (to be implemented)
+router.delete('/:id', (req, res) => {
+    // Your delete logic here
+})
 
 export default router
